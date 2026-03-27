b024125625716f279cb67a6f54aa982448a086dc
Cristian Mendez <cristian.mendez@hemia.mx>
2026-03-26 23:55:08 -0600

    feat(*) update schema

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,12 +2,18 @@ import { betterAuth } from "better-auth";
 import { organization } from "better-auth/plugins"
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../database/database";
+import * as schema from "../database/schema";
 
 export const auth = betterAuth({
      secret: process.env.BETTER_AUTH_SECRET!,
      url: process.env.BETTER_AUTH_URL!,
+     emailAndPassword: {
+        enabled: true,
+     },
+     trustedOrigins: [process.env.ALLOWED_ORIGINS!],
      database: drizzleAdapter(db, {
         provider: "pg",
+        schema: schema
     }),
     plugins: [
         organization()
